0f702fba3c9e58421961d2f2880776da242b2c94
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-05-04 00:17:24 +0200

    JSX: Events Listener

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ const title = "Bonjour les gens";
 const style = {color: 'white', backgroundColor: 'black'};
 
 function App() {
-      return <>
-        <h1 id="title" className="title" style={style}>{title}</h1>
-        <input type="text" />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ad possimus assumenda animi repudiandae velit aspernatur sint, dolores ab quisquam accusamus voluptate molestiae nesciunt beatae natus cum. Veritatis, qui ab.
-        </p>
-      </>
+  const handleClick = () => {
+    alert("J'ai cliquer sur le titre !!!")
+  }
+
+  return <>
+    <h1 onClick={handleClick} id="title" className="title" style={style}>{title}</h1>
+    <input type="text" />
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ad possimus assumenda animi repudiandae velit aspernatur sint, dolores ab quisquam accusamus voluptate molestiae nesciunt beatae natus cum. Veritatis, qui ab.
+    </p>
+  </>
 }
 
 export default App;
