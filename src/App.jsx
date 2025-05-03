6fe6f4c4f15133c192b5cfc285967427b145a45c
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-05-04 00:12:04 +0200

    JSX: variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+const title = "Bonjour les gens";
+const style = {color: 'white', backgroundColor: 'black'};
+
 function App() {
-  return "Bonjour les gens";
+      return <>
+        <h1 id="title" className="title" style={style}>{title}</h1>
+        <input type="text" />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ad possimus assumenda animi repudiandae velit aspernatur sint, dolores ab quisquam accusamus voluptate molestiae nesciunt beatae natus cum. Veritatis, qui ab.
+        </p>
+      </>
 }
 
 export default App;
