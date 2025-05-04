ed1b8600614994e0123db76a3ec04613ab3bda2d
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-05-04 12:35:55 +0200

    JSX: Les composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-const title = "Bonjour les gens";
-const style = {color: 'white', backgroundColor: 'black'};
-const showTitle = false;
+// const title = "Bonjour les gens";
+// const style = {color: 'white', backgroundColor: 'black'};
+// const showTitle = false;
 const todos = [
   'Présenter react',
   'Présenter le JSX', 
@@ -13,7 +13,9 @@ function App() {
   } */
 
   return <>
-    {showTitle && <h1 /* onClick={handleClick} */ id="title" className="title" style={style}>{title}</h1>}
+    <Title color="red" /* hidden */>
+      <p>SAlut</p>
+    </Title>
     <input type="text" />
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ad possimus assumenda animi repudiandae velit aspernatur sint, dolores ab quisquam accusamus voluptate molestiae nesciunt beatae natus cum. Veritatis, qui ab.
@@ -24,6 +26,19 @@ function App() {
       })}
     </ul>
   </>
+}
+
+function Title({color, children, hidden}) {
+  if (hidden) {
+    return null;
+  }
+
+  const props = {
+    id: 'monid',
+    className: 'maClasse'
+  }
+
+  return <h1 style={{color: color}} {...props} >{children}</h1>
 }
 
 export default App;
